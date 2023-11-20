8c8e09e37c2c063eaf106c156ff506166a8b060d
Great Ogheneochuko <greatochuko123@gmail.com>
2023-11-20 23:44:09 +0100

    created and styled settings dropdown menu

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const notificationsDropdown = document.getElementById("notifications-dropdown");
+const settingsDropdown = document.getElementById("settings-dropdown");
 
 function toggleNotificationsDropdown() {
   notificationsDropdown.classList.toggle("open");
+}
+
+function toggleSettingsDropdown() {
+  settingsDropdown.classList.toggle("open");
 }
